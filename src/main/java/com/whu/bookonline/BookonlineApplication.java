ebd4a4d2da3a0f43e682b51668e3596921b4f9c9
siberiawolfp <siberiawolfp@gmail.com>
2020-03-08 13:02:58 +0800

    init update

---
+++ src/main/java/com/whu/bookonline/BookonlineApplication.java
@@ -0,0 +1,13 @@
+package com.whu.bookonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookonlineApplication.class, args);
+    }
+
+}
